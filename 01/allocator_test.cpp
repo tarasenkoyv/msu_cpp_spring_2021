9e223c900c29c186856c8dd2d249fa3c4753082c
tarasenko.yv <tarasenko.yv@gmail.com>
2021-04-02 00:51:52 +0300

    Fixed notes 1.

--- 01/allocator_test.cpp
+++ 01/allocator_test.cpp
@@ -25,65 +25,57 @@ int main()
 * when the function makeAllocator is not called.
 */
 void AllocWithoutMakeAllocatorCall() {
-    Allocator* allocator = new Allocator();
-    
-    assert(allocator->alloc(1) == nullptr);
+    Allocator allocator;
 
-    delete allocator;
+    assert(allocator.alloc(1) == nullptr);
 }
 
 /**
 * Allocation of memory size exceeding the maximum size of the Allocator instance.
 */
 void AllocMemorySizeExceedingMaxSizeTest() {
-    Allocator* allocator = new Allocator();
+    Allocator allocator;
 
-    allocator->makeAllocator(5);
-    assert(allocator->alloc(6) == nullptr);
-
-    delete allocator;
+    allocator.makeAllocator(5);
+    assert(allocator.alloc(6) == nullptr);
 }
 /**
 * Re-allocation memory with the makeAllocator function.
 */
 void ReAllocMemoryTest() {
-    Allocator* allocator = new Allocator();
+    Allocator allocator;
 
-    allocator->makeAllocator(5);
-    assert(allocator->alloc(1) != nullptr);
-    assert(allocator->get_offset() == 1);
-    allocator->makeAllocator(5);
-    assert(allocator->get_max_size() == 5);
-    assert(allocator->get_offset() == 0);
-
-    delete allocator;
+    allocator.makeAllocator(5);
+    assert(allocator.alloc(1) != nullptr);
+    assert(allocator.get_offset() == 1);
+    allocator.makeAllocator(5);
+    assert(allocator.get_max_size() == 5);
+    assert(allocator.get_offset() == 0);
 }
 /**
 * Testing Allocator variable values for correctness.
 */
 void CheckBoundaryConditionsTest() {
-    Allocator* allocator = new Allocator();
+    Allocator allocator;
 
-    allocator->makeAllocator(0);
-    assert(allocator->get_max_size() == 0);
-    assert(allocator->get_offset() == 0);
-    assert(allocator->alloc(10) == nullptr);
+    allocator.makeAllocator(0);
+    assert(allocator.get_max_size() == 0);
+    assert(allocator.get_offset() == 0);
+    assert(allocator.alloc(10) == nullptr);
 
-    allocator->makeAllocator(10);
-    assert(allocator->get_max_size() == 10);
-    assert(allocator->get_offset() == 0);
+    allocator.makeAllocator(10);
+    assert(allocator.get_max_size() == 10);
+    assert(allocator.get_offset() == 0);
 
-    assert(allocator->alloc(10) != nullptr);
-    assert(allocator->get_offset() == 10);
+    assert(allocator.alloc(10) != nullptr);
+    assert(allocator.get_offset() == 10);
 
-    assert(allocator->alloc(1) == nullptr);
-    assert(allocator->get_offset() == 10);
-    assert(allocator->get_max_size() == 10);
+    assert(allocator.alloc(1) == nullptr);
+    assert(allocator.get_offset() == 10);
+    assert(allocator.get_max_size() == 10);
 
-    allocator->reset();
-    assert(allocator->get_offset() == 0);
-    assert(allocator->get_max_size() == 10);
-    assert(allocator->alloc(10) != nullptr);
-
-    delete allocator;
+    allocator.reset();
+    assert(allocator.get_offset() == 0);
+    assert(allocator.get_max_size() == 10);
+    assert(allocator.alloc(10) != nullptr);
 }
