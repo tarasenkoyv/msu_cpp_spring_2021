29875dad8351e402275b0dff5456e6844c915dba
tarasenko.yv <tarasenko.yv@gmail.com>
2021-04-02 18:38:13 +0300

    Added ParseDigit_UINT_MAX_Test.

--- 02/token_parser.cpp
+++ 02/token_parser.cpp
@@ -48,9 +48,8 @@ inline bool TokenParser::IsDigit(const std::string& token) {
     std::regex isDigit{ R"(\d+)" };
     if (std::regex_match(token, isDigit)) {
         try {
-            size_t pos;
-            std::stoul(token, &pos);
-            return pos == token.size();
+            std::stoul(token);
+            return true;
         }
         catch (const std::exception &e) {
             return false;
