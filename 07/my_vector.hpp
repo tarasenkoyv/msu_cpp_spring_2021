f7fc4a8ca061a11dbb2c5d0e52310364fa3e541e
tarasenko.yv <tarasenko.yv@gmail.com>
2021-05-07 17:39:59 +0300

    Done hw07.

---
+++ 07/my_vector.hpp
@@ -0,0 +1,224 @@
+#pragma once
+
+template <class T>
+class Allocator {
+
+public:
+	Allocator() {}
+	~Allocator() {}
+	using value_type = T;
+	using pointer = T*;
+
+	pointer allocate(size_t count) {
+		if (count == 0) {
+			return nullptr;
+		}
+		return new value_type[count];
+	}
+
+	void construct(pointer ptr, const value_type& val) {
+		new (ptr) value_type(val);
+	}
+
+	void deallocate(pointer ptr) {
+		delete[] ptr;
+	}
+
+	void destruct(pointer ptr, size_t count) {
+
+		for (size_t i = 0; i < count; ++i) {
+			ptr[i].~value_type();
+		}
+	}
+
+	size_t max_size() const noexcept {
+		return std::numeric_limits<size_t>::max();
+	}
+};
+
+template <class T>
+class Iterator {
+private:
+	T* ptr_;
+	bool rev_f_;
+public:
+	using difference_type = std::ptrdiff_t;
+	using reference = T&;
+	explicit Iterator(T* ptr, bool rev = false) : ptr_(ptr), rev_f_(rev) {}
+
+	Iterator& operator++() {
+
+		if (rev_f_)
+			--ptr_;
+		else
+			++ptr_;
+
+		return *this;
+	}
+
+	Iterator operator+(size_t n) const {
+
+		if (rev_f_)
+			return Iterator(ptr_ - n);
+		else
+			return Iterator(ptr_ + n);
+	}
+
+	difference_type operator-(const Iterator<T>& other) const
+	{
+		return std::distance(ptr_, other.ptr_);
+	}
+
+	Iterator operator-(size_t n) const {
+
+		if (rev_f_)
+			return Iterator(ptr_ + n);
+		else
+			return Iterator(ptr_ - n);
+	}
+
+	Iterator& operator--() {
+
+		if (rev_f_)
+			ptr_++;
+		else
+			ptr_--;
+
+		return *this;
+	}
+
+	bool operator==(const Iterator<T>& other) const {
+
+		return ptr_ == other.ptr_;
+	}
+
+	bool operator!=(const Iterator<T>& other) const {
+
+		return !(*this == other);
+	}
+
+	reference operator*() const {
+
+		return *ptr_;
+	}
+
+};
+
+template <class T, class Alloc = Allocator<T>>
+class Vector {
+private:
+	size_t size_;
+	Alloc alloc_;
+	T* ptr_;
+	size_t capacity_;
+public:
+	using iterator = Iterator<T>;
+
+	Vector() : size_(0), ptr_(alloc_.allocate(0)), capacity_(0) {}
+
+	Vector(size_t size) : size_(size), ptr_(alloc_.allocate(size)), capacity_(size) {}
+
+	Vector(const std::initializer_list<T>& list) : Vector(list.size()) {
+		size_t count = 0;
+		for (auto& element : list) {
+			ptr_[count] = element;
+			++count;
+		}
+	}
+
+	~Vector() {
+		clear();
+		alloc_.deallocate(ptr_);
+	}
+
+	void resize(size_t new_size) {
+		if (new_size < size_) {
+			alloc_.destruct(ptr_ + new_size, size_ - new_size);
+			size_ = new_size;
+		}
+		else
+			if (new_size > size_) {
+				if (new_size > capacity_)
+					reserve(new_size);
+				for (size_t i = size_; i < new_size; ++i)
+					alloc_.construct(ptr_ + i, T());
+				size_ = new_size;
+			}
+	}
+
+	void reserve(size_t new_cap) {
+		if (new_cap > capacity_) {
+			T* new_ptr = alloc_.allocate(new_cap);
+			for (size_t i = 0; i < size_; ++i)
+				alloc_.construct(new_ptr + i, std::move(ptr_[i]));
+			alloc_.destruct(ptr_, size_);
+			alloc_.deallocate(ptr_);
+			ptr_ = new_ptr;
+			capacity_ = new_cap;
+		}
+	}
+
+	void clear() {
+		alloc_.destruct(ptr_, size_);
+		size_ = 0;
+	}
+
+	void push_back(const T& val) {
+		if (capacity_ == size_) {
+			reserve(size_ + 1);
+		}
+		alloc_.construct(ptr_ + size_, val);
+		size_++;
+	}
+
+	void pop_back() {
+		if (size_ > 0) {
+			size_--;
+			alloc_.destruct(ptr_ + size_, 1);
+		}
+	}
+
+	const T& operator[](size_t i) const {
+		if (i >= size_) {
+			throw std::out_of_range("Invalid index");
+		}
+		return ptr_[i];
+	}
+
+	T& operator[](size_t i) {
+		if (i >= size_) {
+			throw std::out_of_range("Invalid index");
+		}
+		return ptr_[i];
+	}
+
+	size_t size() const {
+		return size_;
+	}
+
+	size_t capacity() const {
+		return capacity_;
+	}
+
+	bool empty() const {
+		return (size_ == 0);
+	}
+
+	iterator begin() {
+		return Iterator<T>(ptr_);
+	}
+
+	iterator end() {
+		return Iterator<T>(ptr_ + size_);
+	}
+
+	iterator rbegin() {
+		return Iterator<T>(ptr_ + size_ - 1, true);
+
+	}
+
+	iterator rend() {
+		return Iterator<T>(ptr_ - 1, true);
+	}
+};
+
---
+++ 07/my_vector_test.cpp
@@ -0,0 +1,164 @@
+#include <cassert>
+#include <iostream>
+#include <algorithm>
+#include <sstream>
+
+#include "my_vector.hpp"
+
+
+void ConsistentTest();
+void ForwardIterTest();
+void ReverseIterTest();
+void InitializeListTest();
+
+int main()
+{
+	ConsistentTest();
+	ForwardIterTest();
+	ReverseIterTest();
+	InitializeListTest();
+
+	std::cout << "Success!" << std::endl;
+
+	return 0;
+}
+
+void ConsistentTest() {
+	Vector<int> my_vec;
+
+	assert(my_vec.empty());
+	assert(my_vec.size() == 0);
+
+	my_vec.push_back(2);
+
+	assert(!my_vec.empty());
+	assert(my_vec.size() == 1);
+	assert(my_vec[0] == 2);
+
+	my_vec.pop_back();
+
+	assert(my_vec.empty());
+	assert(my_vec.size() == 0);
+
+	my_vec.push_back(1);
+	my_vec.push_back(2);
+	my_vec.push_back(3);
+	my_vec.push_back(4);
+
+	assert(!my_vec.empty());
+	assert(my_vec.size() == 4);
+	assert(my_vec[0] == 1);
+	assert(my_vec[1] == 2);
+	assert(my_vec[2] == 3);
+	assert(my_vec[3] == 4);
+
+	auto rev_iter = my_vec.rbegin();
+	assert(rev_iter != my_vec.rend());
+	assert(*rev_iter == 4);
+	++rev_iter;
+	assert(rev_iter != my_vec.rend());
+	assert(*rev_iter == 3);
+	++rev_iter;
+	assert(rev_iter != my_vec.rend());
+	assert(*rev_iter == 2);
+	++rev_iter;
+	assert(rev_iter != my_vec.rend());
+	assert(*rev_iter == 1);
+	++rev_iter;
+	assert(rev_iter == my_vec.rend());
+
+	auto forward_iter = my_vec.begin();
+	assert(forward_iter != my_vec.end());
+	assert(*forward_iter == 1);
+	++forward_iter;
+	assert(forward_iter != my_vec.end());
+	assert(*forward_iter == 2);
+	++forward_iter;
+	assert(forward_iter != my_vec.end());
+	assert(*forward_iter == 3);
+	++forward_iter;
+	assert(forward_iter != my_vec.end());
+	assert(*forward_iter == 4);
+	++forward_iter;
+	assert(forward_iter == my_vec.end());
+
+	my_vec.reserve(100);
+	assert(my_vec.size() == 4);
+	assert(my_vec.capacity() >= 100);
+
+	const auto capacity = my_vec.capacity();
+
+	my_vec.resize(2);
+	assert(my_vec.size() == 2);
+	assert(my_vec.capacity() == capacity);
+	assert(my_vec[0] == 1);
+	assert(my_vec[1] == 2);
+
+	my_vec.resize(3);
+	assert(my_vec.size() == 3);
+	assert(my_vec.capacity() == capacity);
+	assert(my_vec[0] == 1);
+	assert(my_vec[1] == 2);
+	assert(my_vec[2] == 0);
+
+	my_vec.resize(4);
+	assert(my_vec.size() == 4);
+	assert(my_vec.capacity() == capacity);
+	assert(my_vec[0] == 1);
+	assert(my_vec[1] == 2);
+
+	my_vec.resize(0);
+	assert(my_vec.size() == 0);
+	assert(my_vec.begin() == my_vec.end());
+
+	my_vec.resize(2);
+	assert(my_vec.size() == 2);
+}
+
+void ForwardIterTest() {
+
+	struct StrOut
+	{
+		void operator()(int n) { 
+			value += std::to_string(n) + " ";
+		}
+		std::string value = "";
+	};
+
+	Vector<int> my_vec = { 9, 8, 1, 3, 4, 5, 6 };
+	StrOut str_out = std::for_each(my_vec.begin(), my_vec.end(), StrOut());
+
+	assert(str_out.value == "9 8 1 3 4 5 6 ");
+}
+
+void ReverseIterTest() {
+
+	struct StrOut
+	{
+		void operator()(int n) {
+			value += std::to_string(n) + " ";
+		}
+		std::string value = "";
+	};
+
+	Vector<int> my_vec = { 9, 8, 1, 3, 4, 5, 6 };
+	StrOut str_out = std::for_each(my_vec.rbegin(), my_vec.rend(), StrOut());
+
+	assert(str_out.value == "6 5 4 3 1 8 9 ");
+}
+
+void InitializeListTest() {
+	Vector<int> my_vec = { 9, 8, 1, 3, 4, 5, 6 };
+
+	assert(my_vec[0] == 9);
+	assert(my_vec[1] == 8);
+	assert(my_vec[2] == 1);
+	assert(my_vec[3] == 3);
+	assert(my_vec[4] == 4);
+	assert(my_vec[5] == 5);
+	assert(my_vec[6] == 6);
+
+	assert(my_vec.size() == 7);
+	assert(my_vec.capacity() == 7);
+	assert(!my_vec.empty());
+}
