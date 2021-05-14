ef5a7bc2b49670e0503a07e189ba84a7acf55533
tarasenko.yv <tarasenko.yv@gmail.com>
2021-05-14 17:24:16 +0300

    Done hw08.

---
+++ 08/pool.hpp
@@ -0,0 +1,64 @@
+#pragma once
+
+#include <thread>
+#include <future>
+#include <queue>
+#include <vector>
+#include <functional>
+
+class ThreadPool
+{
+private:
+    // Indicates that a destructor is running
+    bool is_destructing_;
+    std::condition_variable condition_;
+    std::mutex mutex_;
+    std::queue <std::function<void()>> tasks_;
+    std::vector <std::thread> threads_;
+
+
+
+public:
+    explicit ThreadPool(size_t poolSize) : is_destructing_(false) {
+        for (size_t i = 0; i < poolSize; ++i) {
+            threads_.emplace_back(
+                [this]() {
+                    while (!is_destructing_) {
+                        std::unique_lock <std::mutex> lock(mutex_);
+                        while (!is_destructing_ && tasks_.empty()) {
+                            // Predicate returns false, the method unlocks the mutex
+                            // and goes to sleep until it receives a notification.
+                            condition_.wait(lock);
+                        }
+                        
+                        if (is_destructing_ || tasks_.empty()) break;
+                        auto task = tasks_.front();
+                        tasks_.pop();
+                        task();
+                    }
+                }
+            );
+        }
+    }
+
+
+    ~ThreadPool() {
+        is_destructing_ = true;
+        condition_.notify_all();
+        for (auto& t : threads_) {
+            t.join();
+        }
+    }
+
+
+    template <class Func, class... Args>
+    auto exec(Func func, Args... args) -> std::future<decltype(func(args...))> {
+        auto task = std::make_shared<std::packaged_task<decltype(func(args...))()>>
+            (std::bind(func, args...));
+        std::unique_lock<std::mutex> lock(mutex_);
+        tasks_.push([task]() { (*task)(); });
+        condition_.notify_one();
+
+        return task->get_future();
+    }
+};
---
+++ 08/pool_test.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <assert.h>
+#include <chrono>
+#include <thread>
+
+#include "pool.hpp"
+
+int foo();
+
+void ExecutionTest();
+
+int main() {
+    ExecutionTest();
+
+    std::cout << "Success!" << std::endl;
+
+    return 0;
+}
+
+void ExecutionTest() {
+    ThreadPool pool(8);
+    auto t1 = pool.exec(foo);
+    for (int i = 0; i < 10; ++i) {
+        assert(pool.exec([i]() { return i; }).get() == i);
+    }
+
+    assert(t1.get() == 0);
+}
+
+int foo() {
+    std::this_thread::sleep_for(std::chrono::seconds(2));
+    return 0;
+}
